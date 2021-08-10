39443f415663bd062c1e2e11249ce7eef496c7ce
Titu YooKyung Kim <tituvely@gmail.com>
2021-08-10 22:03:50 +0900

    v-on

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,24 @@
   <div class="menu">
     <a v-for="menu in menus" :key="menu">{{ menu }}</a>
   </div>
-  <div v-for="product in products" :key="product">{{ product }}</div>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ prices[0] }}만원</p>
+    <button v-on:click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ reportCount[0] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ prices[1] }}만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ reportCount[1] }}</span>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ prices[2] }}만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ reportCount[2] }}</span>
+  </div>
 </template>
 
 <script>
@@ -10,9 +27,16 @@ export default {
   name: "App",
   data() {
     return {
+      reportCount: [0, 0, 0],
+      prices: [50, 60, 70],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       menus: ["Home", "Products", "About"],
     };
+  },
+  methods: {
+    increase(index) {
+      this.reportCount[index]++;
+    },
   },
   components: {},
 };
