b80a21397bbcde07f4a932e952ad926fc11ca193
Titu YooKyung Kim <tituvely@gmail.com>
2021-08-10 21:52:13 +0900

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div>
-    <h4 v-bind:style="styleBlue">{{ products[0] }}</h4>
-    <p>{{ price1 }} 만원</p>
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
   </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
+  <div v-for="product in products" :key="product">{{ product }}</div>
 </template>
 
 <script>
@@ -15,10 +10,8 @@ export default {
   name: "App",
   data() {
     return {
-      price1: 60,
-      price2: 80,
-      styleBlue: "color : blue",
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      menus: ["Home", "Products", "About"],
     };
   },
   components: {},
@@ -33,5 +26,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
