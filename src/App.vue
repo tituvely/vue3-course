a68bab2ddc3856de2955a2352ee52e231fef3217
Titu YooKyung Kim <tituvely@gmail.com>
2021-08-11 15:53:01 +0900

    vue3 - ref/reactive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,57 @@
 <template>
-  <div></div>
+  <div class="name">{{ name }}</div>
+  <div>{{ greeting(name) }}</div>
+  <div>{{ greet }}</div>
+  <div>{{ user }}</div>
+  <div>{{ city }}</div>
+  <button class="btn btn-primary" @click="updateName">Click</button>
 </template>
 
 <script>
-export default {};
+import { ref, reactive } from "vue";
+
+export default {
+  setup() {
+    const name = ref("Titu Coder");
+    const city = ref({
+      name: "seoul",
+    });
+    //reactive는 obejct, array만 사용 가능
+    const user = reactive({
+      id: 1,
+    });
+
+    const greeting = (name) => {
+      return "hello, " + name;
+    };
+
+    const greet = greeting(name.value);
+
+    const consoleLog = () => {
+      console.log("hello world");
+    };
+
+    const updateName = () => {
+      name.value = "Titu";
+      city.value.name = "Paris";
+      user.id = 2;
+    };
+
+    return {
+      name,
+      greeting,
+      greet,
+      consoleLog,
+      updateName,
+      user,
+      city,
+    };
+  },
+};
 </script>
 
 <style>
+.name {
+  color: red;
+}
 </style>
